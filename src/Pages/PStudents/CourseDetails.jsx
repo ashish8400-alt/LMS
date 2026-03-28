03d0fece20786bfdc807770490aa204652e8c830
Ashish Tiwari <ashishtiwari1314q@gmail.com>
2026-03-28 17:32:16 +0530

    Work on CourseList

--- src/Pages/PStudents/CourseDetails.jsx
+++ src/Pages/PStudents/CourseDetails.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../../Context/AppContext'
 
 const CourseDetails = () => {
+
+const {id} = useParams()
+
+const [courseData, setcourseData] = useState(null)
+
+const {allCourses}  = useContext(AppContext)
+
+const fetCourseData = async ()=>{
+ const findCourse =  allCourses.find(course =>course._id === id)
+ setcourseData(findCourse);
+}
+
+useEffect(()=>{
+  fetCourseData()
+},[])
+
   return (
-    <div>
-      <h1>Course  Details</h1>
+    <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
+      
+{/* left column */}
+<div></div>
+
+{/* right column */}
+<div></div>
+
+
     </div>
   )
 }
--- src/Pages/PStudents/CoursesList.jsx
+++ src/Pages/PStudents/CoursesList.jsx
@@ -1,15 +1,79 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../../Context/AppContext'
+import SearchBar from '../../Components/Students/SearchBar'
+import { useParams } from 'react-router-dom'
+import CourseCard from '../../Components/Students/CourseCard'
+import { assets } from '../../assets/assets'
+import Footer from '../../Components/Students/Footer'
 
 const CoursesList = () => {
+
+
+
+  const { navigate, allCourses } = useContext(AppContext)
+
+  // ye padhna hai
+
+  const { input } = useParams()
+
+  // ye bana rhe hai kisi chij ko search karne keliye
+
+  const [filteredCourse, setfilteredCourse] = useState([])
+
+  useEffect(() => {
+    if (allCourses && allCourses.length > 0) {
+      const tempCourses = allCourses.slice()
+
+      input ?
+        setfilteredCourse(
+          tempCourses.filter(
+            item => item.courseTitle.toLowerCase().includes(input.toLocaleLowerCase())
+          )
+        )
+        : setfilteredCourse(tempCourses)
+    }
+  }, [allCourses, input])
+
+
+
+
   return (
     <>
-   <div className='mt-20'>
-    <div>
-      <h1>Course List</h1>
-      <p>
-        <span>Home</span> / Course List</p>
-    </div>
-   </div>
+      <div className='mt-20 realative md:px-36 px-8 pt-20 text-left'>
+        <div className='flex md:flex-row flex-col gap-6 items-start justify-between w-full'>
+          <div>
+            <h1 className='text-4xl font-semibold text-gray-800'>Course List</h1>
+            <p className='text-gray-500 '>
+              <span className='text-blue-600 cursor-pointer'
+                onClick={() => navigate('/')}
+              >Home</span> / Course List</p>
+          </div>
+          <SearchBar data={input} />
+        </div>
+
+
+{/* ye hai ki aap kya search kiya hai  */}
+
+        {input && <div className='inline-flex items-center gap-4 px-4 py-2 border mt-8 -mb-8 text-gray-600'>
+          <p>{input}</p>
+          <img src={assets.cross_icon} alt="" className='cursor-pointer' onClick={() =>
+            navigate('/course-list')
+          } />
+        </div>
+        }
+
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0' >
+
+
+          {/* for display the course */}
+
+
+          {filteredCourse.map((course, index) => <CourseCard key={index} course={course} />)}
+
+        </div>
+
+      </div>
+      <Footer/>
     </>
   )
 }
