8ae007f2da4ba7a34556210e935f2a97c99c8c54
Ashish Tiwari <ashishtiwari1314q@gmail.com>
2026-04-03 01:15:14 +0530

    Work on course Details Page (Video Section)

--- src/Pages/PStudents/MyEnrollments.jsx
+++ src/Pages/PStudents/MyEnrollments.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const MyEnrollments = () => {
   return (
-    <div>
+    <div className='mt-20'>
       <h1>My Enrollments Page</h1>
+      <table>
+        <thead>
+          <tr>
+            <th className='px-4 py-3  font-semibold truncate'>Course</th>
+             <th className='px-4 py-3  font-semibold truncate'>Duration</th>
+              <th className='px-4 py-3  font-semibold truncate'>Completed</th>
+               <th className='px-4 py-3  font-semibold truncate'>Status</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   )
 }
